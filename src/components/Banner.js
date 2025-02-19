0006aad7bf81546dc9cf79f9c40e905c5e0a2a4c
PrinceRaj8 <princeraj7g5@gmail.com>
2025-02-19 13:01:46 +0530

    Create Banner.js

---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <img src="https://picsum.photos/1200/400?random=10" alt="Amazon Banner" />
+    </div>
+  );
+}
+
+export default Banner;
